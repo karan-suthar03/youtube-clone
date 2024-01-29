904f79dbc6a56d34213c09257b7df6b915423f9d
karan <karan.suthar.lakha@gmail.com>
2024-01-30 01:26:19 +0530

    store description and likes

--- data/data.js
+++ data/data.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const fs = require('fs');
 const router = express.Router();
+let VDetails = fs.readFileSync(__dirname+ "/discription and likes.json");
+VDetails = JSON.parse(VDetails.toString())
 router.get('/thumbnails/:id', (req, res) => {
     const id = req.params.id;
     res.sendFile(__dirname + '/thumbnails/' + id);
@@ -12,5 +15,10 @@ router.get('/videos/:id', (req, res) => {
     const id = req.params.id;
     console.log(id);
     res.sendFile(__dirname + '/videos/' + id);
+})
+
+router.get('/VDetails/:id', (req, res) => {
+    let id = req.params.id;
+    res.json(VDetails.data[id])
 })
 module.exports = router;
